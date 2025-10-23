d067173ccd52ebf275316709fe953cc4ccd83912
JGMelon22 <jgvasconcellos22@gmail.com>
2025-10-23 13:16:33 -0300

    feat: remove maximize form window and do make form fixed single

--- InvoiceProcessorDesktopLegacy/Form1.Designer.cs
+++ InvoiceProcessorDesktopLegacy/Form1.Designer.cs
@@ -189,8 +189,10 @@
             this.Controls.Add(this.groupBoxBusca);
             this.Controls.Add(this.groupBoxOperacoes);
             this.Controls.Add(this.lblStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema de Importação de Notas Fiscais - UneCont";
